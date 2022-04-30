90dd815d8ece0ea3ae4e788c64c68fc5b5ffd622
David042 <david.mal04@hotmail.com>
2022-04-29 21:14:56 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
+import Calc from './components/Calc';
 
 export default function App() {
+  const [conv, setConv] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <View style={styles.texto}> Digite o número de convidados </View>
+        <TextInput
+          style={styles.input}
+          value={conv}
+          onChangeText={(value)=>setConv(value)}
+        />
+      </View>
+      <View>
+        <Calc conv={conv}/>
+      </View>
     </View>
   );
 }
@@ -17,4 +29,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row:{
+    width:'100%',
+    marginTop:10
+  },
+  input:{
+    width:'80%',
+    borderWidth:1,
+    marginLeft:'10%',
+    fontSize:30,
+    borderRadius:6
+  },
+  texto:{
+    fontSize:30,
+    marginLeft:'10%'
+  }
 });
